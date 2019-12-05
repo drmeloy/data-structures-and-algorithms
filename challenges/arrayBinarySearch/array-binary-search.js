e284d9dbd82cdd88ea0bfd0709df7487b16b5362
drmeloy <drmeloy@gmail.com>
2019-12-05 13:52:54 -0700

    binary sort function working

--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,9 +1,9 @@
 const arrayBinarySearch = (array, value) => {
   let l = 0;
   let h = array.length - 1;
-  let mid = Math.floor((l + h) / 2);
-
+  
   while(l <= h){
+    let mid = Math.floor((l + h) / 2);
     if(array[mid] === value) return mid;
     if(array[mid] > value) h = mid - 1;
     if(array[mid] < value) l = mid + 1;
